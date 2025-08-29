a7095ba0d87b36bf0266f50215a9ef9fa664b482
asammarco <asammarco@hotmail.com>
2025-08-29 00:12:48 -0300

    chore(core): implement main exports index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-function init() {
-	console.log('init')
-}
-init()
+export { ChangeStreamBroker } from './broker/change-stram-broker'
+
+export type {
+	BrokerConfig,
+	ConsumerConfig,
+	ConsumerRecord,
+	ErrorHandler,
+	IChangeStreamConsumer,
+	IChangeStreamProducer,
+	Message,
+	MessageHandler,
+	MessageHandlerConfig,
+	OffsetCommit,
+	ProducerConfig,
+	TopicConfig,
+} from './broker/types'
+export { ChangeStreamConsumer } from './consumer/consumer'
+export { ChangeStreamProducer } from './producer/producer'
+export { OffsetStorage } from './storage/offset-storage'
